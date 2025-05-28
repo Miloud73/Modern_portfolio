267cae50b51078aef967f05d205f48ef906f09fc
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-05-28 13:37:09 +0000

    update hero section

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 function HeroContent() {
@@ -23,13 +27,15 @@ function HeroContent() {
         </motion.div>
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-          Providing
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-            the best
+          <span className="flex flex-col gap-6">
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              the best
+            </span>
+            project experience
           </span>
-          project experience
         </motion.div>
         <motion.p
           variants={slideInFromLeft(0.8)}
@@ -43,19 +49,22 @@ function HeroContent() {
           to database.
         </motion.p>
         <motion.a
-        variants={slideInFromLeft(1)}
-        className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-            Learn more!
-
+          Learn more!
         </motion.a>
       </div>
       <motion.div
-      variants={slideInFromRight(0.8)}
-      className="w-full h-full flex flex-center items-center"
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex flex-center items-center"
       >
-        <Image src="/mainIconsdark.svg" width={650} height={650} alt="work icons" />
-
+        <Image
+          src="/mainIconsdark.svg"
+          width={650}
+          height={650}
+          alt="work icons"
+        />
       </motion.div>
     </motion.div>
   );
