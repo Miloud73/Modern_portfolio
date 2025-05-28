73c773fa7adba7262c97e7d6d2305c7068883828
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-05-28 13:04:16 +0000

    implements hero section

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,26 +1,64 @@
-import React from 'react'
-import {motion} from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 function HeroContent() {
   return (
     <motion.div
-        initial="hidden"
-        animate="visible"
-        className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20] '
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20] "
     >
-        <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start' >
-            <motion.div
-             variants={slideInFromTop}
-             className='Welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
-            >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            React.js | React Native | Next.js Developer Portfolio
+          </h1>
+        </motion.div>
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          Providing
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            the best
+          </span>
+          project experience
+        </motion.div>
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m a dedicated full-stack developer with a strong focus on
+          front-end development, specializing in Next.js, React.js, and React
+          Native. I build modern, responsive web and mobile applications with
+          clean, efficient code. With experience across both front-end and
+          back-end technologies, I deliver complete, scalable solutions from UI
+          to database.
+        </motion.p>
+        <motion.a
+        variants={slideInFromLeft(1)}
+        className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+            Learn more!
 
-            </motion.div>
+        </motion.a>
+      </div>
+      <motion.div
+      variants={slideInFromRight(0.8)}
+      className="w-full h-full flex flex-center items-center"
+      >
+        <Image src="/mainIconsdark.svg" width={650} height={650} alt="work icons" />
 
-        </div>
-
-      
+      </motion.div>
     </motion.div>
-  )
+  );
 }
 
-export default HeroContent
+export default HeroContent;
