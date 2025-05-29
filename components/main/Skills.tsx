4b3f4bd239719857384f3129ca3a28f9645df1ab
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-05-29 05:31:09 +0000

    update skills section

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,4 +1,8 @@
+"use client"
+import { Backend_skill, Frontend_skill, Full_stack, Skill_data } from '@/constants'
 import React from 'react'
+import SkillsDataProvide from '../sub/SkillsDataProvide'
+import SkillsText from '../sub/SkillsText'
 
 const Skills = () => {
   return (
@@ -7,7 +11,65 @@ const Skills = () => {
      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20"
      style={{transform:"scale(0.9)"}}
      >
-        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center"></div>
+      <SkillsText/>
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Skill_data.map((image,index)=>(
+            <SkillsDataProvide
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div> 
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Frontend_skill.map((image,index)=>(
+            <SkillsDataProvide
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div> 
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Backend_skill.map((image,index)=>(
+            <SkillsDataProvide
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div> 
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Full_stack.map((image,index)=>(
+            <SkillsDataProvide
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div>
+        <div className='w-full h-full absolute'>
+          <div className='w-full h-full z-[-10] opawity-30 absolute flex items-center justify-center bg-cover'>
+            <video
+              className='w-full h-auto'
+              preload='false'
+              playsInline
+              loop
+              muted
+              autoPlay
+            />
+
+          </div>
+
+        </div>
       
     </section>
   )
--- components/sub/SkillsDataProvide.tsx
+++ components/sub/SkillsDataProvide.tsx
@@ -1,10 +1,46 @@
+"use client"
 import React from 'react'
+import {motion} from 'framer-motion'
+import {useInView} from 'react-intersection-observer'
+import Image from 'next/image'
 
-const SkillsDataProvide = () => {
+
+interface Props{
+  src : string,
+  width : number,
+  height : number,
+  index : number,
+}
+const SkillsDataProvide = (src , width , height , index) => {
+
+  const [ref,InView] = useInView({
+    triggerOnce: true
+  })
+
+  const imageVariants={
+    hidden:{opacity:0},
+    visible : {opacity:1}
+  }
+
+  const animationDelay = 0.3
+
   return (
-    <div>
+    <motion.div
+      ref={ref}
+      initial = "hidden"
+      variants={imageVariants}
+      animate={InView ? "visible":"hidden"}
+      custom = {index}
+      transition={{delay: index * animationDelay} }
+    >
+      <Image
+        src={src}
+        width={width}
+        height={height}
+        alt="skill images"
+      />
       
-    </div>
+    </motion.div>
   )
 }
 
