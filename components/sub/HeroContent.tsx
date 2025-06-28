cf67a8947f9302e06e3f7a8e7507ad07e2b8c2f7
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2025-06-28 19:53:11 +0000

    update

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -18,10 +18,10 @@ function HeroContent() {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b]  opacity-[0.9] max-md:ml-[10px]"
+          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b]  opacity-[0.9] max-md:ml-[13px]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5 " />
-          <h1 className="Welcome-text text-[13px]">
+          <h1 className="Welcome-text text-[10px]">
             React.js | React Native | Next.js Developer Portfolio
           </h1>
         </motion.div>
